cbf923cabc8332fa287e1163afae2d54bcef4681
Holger Banko <h.banko@gmail.com>
2019-12-14 20:09:51 +1100

    Initial Commit

---
+++ execShell/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  execShell
+//
+//  Created by Holger Banko on 14/12/19.
+//  Copyright © 2019 Holger Banko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
